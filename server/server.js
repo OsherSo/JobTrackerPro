7de456810f0d30315dc972a06096c57d733024e9
Osher Solimany <solimani.osher@gmail.com>
2023-05-15 11:03:41 +0300

    Delete DB folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-const connectDB = require('./db/connect');
 const app = require('./app');
 
 dotenv.config();
@@ -9,7 +9,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     const server = http.createServer(app);
     server.listen(port, () => {
       console.log(`Listening on port ${port}...`);
