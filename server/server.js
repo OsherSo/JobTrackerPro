45e32e5ed8df4331ad569df6a5ceadddecdeb26a
Osher Solimany <solimani.osher@gmail.com>
2023-05-15 10:15:55 +0300

    dotenv config

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const notFound = require("./middleware/notFound");
-const errorHandler = require("./middleware/errorHandler");
+const dotenv = require('dotenv');
+const express = require('express');
+
+const notFound = require('./middleware/notFound');
+const errorHandler = require('./middleware/errorHandler');
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome!");
+app.get('/', (req, res) => {
+  res.send('Welcome!');
 });
 
 app.use(notFound);
