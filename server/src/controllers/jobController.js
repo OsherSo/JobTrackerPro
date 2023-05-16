c41fbd730c8134cd191ae2fafae8620755ebedb4
Osher Solimany <solimani.osher@gmail.com>
2023-05-16 12:25:55 +0300

    Add Job Model and createJob function

--- server/src/controllers/jobController.js
+++ server/src/controllers/jobController.js
@@ -1,19 +1,24 @@
-exports.createJob = (req, res, next) => {
-  res.send('Create Job');
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+
+exports.createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
-exports.deleteJob = (req, res, next) => {
+exports.deleteJob = (req, res) => {
   res.send('Delete Job');
 };
 
-exports.getAllJobs = (req, res, next) => {
+exports.getAllJobs = (req, res) => {
   res.send('Get All Jobs');
 };
 
-exports.updateJob = (req, res, next) => {
+exports.updateJob = (req, res) => {
   res.send('Update Job');
 };
 
-exports.showStats = (req, res, next) => {
+exports.showStats = (req, res) => {
   res.send('Show Stats');
 };
