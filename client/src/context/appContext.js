175968a45176fa029743deaac9269fa12ac0d473
Osher Solimany <solimani.osher@gmail.com>
2023-05-16 11:34:00 +0300

    Axios Setup

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -33,6 +33,13 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const authFetch = axios.create({
+    baseURL: "/api/v1",
+    headers: {
+      Authorization: `Bearer ${state.token}`,
+    },
+  });
+
   const displayAlert = () => {
     dispatch({
       type: DISPLAY_ALERT,
@@ -94,7 +101,12 @@ const AppProvider = ({ children }) => {
   };
 
   const updateUser = async (currentUser) => {
-    console.log(currentUser);
+    try {
+      const { data } = await authFetch.patch("/auth/updateUser", currentUser);
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   return (
