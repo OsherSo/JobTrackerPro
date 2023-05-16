151eda4551bdcc100ce249d111b4166eea89ca9d
Osher Solimany <solimani.osher@gmail.com>
2023-05-16 13:39:26 +0300

    Add getAllJobs controller

--- server/src/controllers/jobController.js
+++ server/src/controllers/jobController.js
@@ -1,18 +1,23 @@
 const Job = require('../models/Job');
 const { StatusCodes } = require('http-status-codes');
 
+exports.getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+};
+
 exports.createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
+
   res.status(StatusCodes.CREATED).json({ job });
 };
 
 exports.deleteJob = (req, res) => {
   res.send('Delete Job');
-};
-
-exports.getAllJobs = (req, res) => {
-  res.send('Get All Jobs');
 };
 
 exports.updateJob = (req, res) => {
