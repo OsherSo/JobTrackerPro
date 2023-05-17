7a56098fde2a04c11340c0cb58bb827ee2ce3d9c
Osher Solimany <solimani.osher@gmail.com>
2023-05-17 20:43:52 +0300

    Add Stats Frond-end

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,20 +2,28 @@ import Logo from "./Logo";
 import Alert from "./Alert";
 import Navbar from "./Navbar";
 import FormRow from "./FormRow";
+import Loading from "./Loading";
+import StatItem from "./StatItem";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
 import FormRowSelect from "./FormRowSelect";
-import SearchContainer from "./SearchContainer";
 import JobsContainer from "./JobsContainer";
+import StatsContainer from "./StatsContainer";
+import SearchContainer from "./SearchContainer";
+import ChartsContainer from "./ChartsContainer";
 
 export {
   Logo,
   Alert,
   Navbar,
   FormRow,
+  Loading,
   BigSidebar,
   SmallSidebar,
   FormRowSelect,
   SearchContainer,
   JobsContainer,
+  StatsContainer,
+  StatItem,
+  ChartsContainer,
 };
