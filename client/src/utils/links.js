64ab9ed606fb08bb3b32cfbcb4becc4f866332e7
Osher Solimany <solimani.osher@gmail.com>
2023-05-28 13:25:16 +0300

    Add Task Manager Link

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -1,32 +1,39 @@
-import { FaWpforms } from "react-icons/fa";
-import { ImProfile } from "react-icons/im";
-import { MdQueryStats } from "react-icons/md";
-import { IoBarChartSharp } from "react-icons/io5";
+import { FaWpforms } from 'react-icons/fa';
+import { ImProfile } from 'react-icons/im';
+import { MdQueryStats } from 'react-icons/md';
+import { VscChecklist } from 'react-icons/vsc';
+import { IoBarChartSharp } from 'react-icons/io5';
 
 const links = [
   {
     id: 1,
-    text: "stats",
-    path: "/",
+    text: 'stats',
+    path: '/',
     icon: <IoBarChartSharp />,
   },
   {
     id: 2,
-    text: "all jobs",
-    path: "all-jobs",
+    text: 'all jobs',
+    path: 'all-jobs',
     icon: <MdQueryStats />,
   },
   {
     id: 3,
-    text: "add job",
-    path: "add-job",
+    text: 'add job',
+    path: 'add-job',
     icon: <FaWpforms />,
   },
   {
     id: 4,
-    text: "profile",
-    path: "profile",
+    text: 'profile',
+    path: 'profile',
     icon: <ImProfile />,
+  },
+  {
+    id: 5,
+    text: 'task manager',
+    path: 'task-manager',
+    icon: <VscChecklist />,
   },
 ];
 
